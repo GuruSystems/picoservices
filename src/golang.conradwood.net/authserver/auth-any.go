a83484a1fd6eda3e9c2ff47d83fdccdf09d4b4bd
Conrad Wood <cnw@singingcat.net>
2017-11-10 11:27:02 +0000

    .

--- src/golang.conradwood.net/authserver/auth-any.go
+++ src/golang.conradwood.net/authserver/auth-any.go
@@ -11,12 +11,13 @@ type AnyAuthenticator struct {
 }
 
 func (pga *AnyAuthenticator) GetUserDetail(user string) (*auth.User, error) {
-	return auth.User{
+	au := auth.User{
 		FirstName: "john",
 		LastName:  "doe",
 		Email:     "john.doe@microsoft.com",
 		ID:        "1",
 	}
+	return &au, nil
 }
 func (pga *AnyAuthenticator) Authenticate(token string) (string, error) {
 	return "1", nil
